b502a2e86401d0dc5df73b53070766da092babf6
Jevon Hayter <jevonhayter@gmail.com>
2017-01-16 17:10:50 -0500

    Hang Man Program

--- hang_man.rb
+++ hang_man.rb
@@ -1,10 +1,9 @@
-require 'Pry'
-
+# Hangman Game
 class BlankLines
   attr_accessor :lines
 
   def initialize(word)
-    @lines = "_" * word.length
+    @lines = '_' * word.length
   end
 
   def to_s
@@ -13,13 +12,13 @@ class BlankLines
 
   def []=(num, other)
     @lines[num] = other
-  end 
+  end
 
   def update_lines(index, word)
     index.each { |num| @lines[num] = word }
   end
 end
-
+# Host writes the secret word tracks available letters
 class Host
   attr_accessor :secret_word, :alphabets, :blank_lines, :hangman
 
@@ -39,72 +38,77 @@ class Host
   end
 
   def draw_hangman(counter)
-    
-    if counter == 1
-      hangman.head = " O"
-    elsif counter == 2
-      hangman.neck = " ^"
-    elsif counter == 3
-      hangman.left_arm = "/ "
-    elsif counter == 4
-      hangman.right_arm = "\\"
-    elsif counter == 5
-      hangman.body = " |"
-    elsif counter == 6
-      hangman.left_leg = "/ "
-    else
-      hangman.right_leg =   "\\"
-    end
+    answer = case counter
+             when 1 then hangman.head = ' O'
+
+             when 2 then hangman.neck = ' ^'
+
+             when 3 then hangman.left_arm = '/ '
+
+             when 4 then hangman.right_arm = '\\'
+
+             when 5 then hangman.body = ' |'
+
+             when 6 then hangman.left_leg = '/ '
+
+             else
+               hangman.right_leg = '\\'
+             end
+    answer
   end
 
   def display_alphabets
-    puts " REMAINING LETTERS #{alphabets} " 
-    puts ""
+    puts " REMAINING LETTERS #{alphabets} "
+    puts ''
   end
 end
 
+# Player class for person making guesses
 class Player
   attr_reader :guess
 
   def guess
-   @guess = gets.chomp.to_s.downcase
+    @guess = gets.chomp.to_s.downcase
   end
 end
 
+# Class to build and display the hangman
 class Hangman
-  attr_accessor :display_hangman, :head, :neck, :left_arm, :right_arm, :body, :left_leg, :right_leg
-  
+  attr_accessor :display_hangman, :head, :neck, :left_arm,
+                :right_arm, :body, :left_leg, :right_leg
+
   def initialize
-    @head = ""
-    @neck = ""
-    @left_arm = ""
-    @right_arm = ""
-    @body = ""
-    @left_leg = ""
-    @right_leg = ""
+    @head = ''
+    @neck = ''
+    @left_arm = ''
+    @right_arm = ''
+    @body = ''
+    @left_leg = ''
+    @right_leg = ''
   end
 
   def display_hangman
-    puts "---------" "\n"
-    puts " |"     
+    puts '--------- \n'
+    puts ' |'
     puts  head
     puts  neck
-    print left_arm 
-    puts right_arm
+    print left_arm
+    puts  right_arm
     puts  body
-    print  left_leg
+    print left_leg
     puts  right_leg
-    puts ""
+    puts ''
   end
 end
 
+# Hangman game engine
 class Game
   attr_reader :host, :player
 
   def display_welcome_message
     puts " 'Welcome to Tooties Hangman' "
-    puts "______________________________"
-    puts ""
+    puts '______________________________'
+    puts ''
     puts " 'Host please choose a secret word' "
   end
 
@@ -139,10 +143,11 @@ class Game
         host.hangman.display_hangman
         puts host.blank_lines
       end
+      puts host.blank_lines
 
-      puts "You Win!" if host.blank_lines.lines == host.secret_word
-      puts "You Lose!" if guess_counter == 7
-      
+      puts 'You Win!' if host.blank_lines.lines == host.secret_word
+      puts 'You Lose!' if guess_counter == 7
+
       break if host.blank_lines.lines == host.secret_word
       break if guess_counter == 7
     end
@@ -157,22 +162,19 @@ class Game
       host.draw_blank_lines
       player_guesses
       puts "The secret word was '#{host.secret_word}'"
-      
+
       answer = nil
       loop do
-        puts "Would you like to play again? (y/n)"
+        puts 'Would you like to play again? (y/n)'
         answer = gets.chomp.downcase
         break if %w(y n).include? answer
-        puts "Sorry, must be y or n"
+        puts 'Sorry, must be y or n'
       end
 
       break unless answer == 'y'
     end
-    puts "Thanks for playing!"
+    puts 'Thanks for playing!'
   end
 end
 
-game = Game.new.play
-
- 
-
+Game.new.play
